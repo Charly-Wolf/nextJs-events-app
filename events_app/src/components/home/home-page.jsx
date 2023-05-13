500ce9f32991cc7d2b6eb8eb0321fc69622991e0
CharlyWolf <cardan1990@hotmail.com>
2023-05-13 12:19:04 +0200

    Added more styles

--- events_app/src/components/home/home-page.jsx
+++ events_app/src/components/home/home-page.jsx
@@ -5,8 +5,10 @@ export const HomePage = ({ data }) => (
     <div className='home_body'>
         {data.map((ev) => (
             <Link className='card' key={ev.id} href={`/events/${ev.id}`}> 
-                <Image alt={ev.title} width={300} 
-                        height={200} src={ev.image}/> 
+            <div className='image'>
+                <Image alt={ev.title} width={500} 
+                        height={350} src={ev.image}/> 
+            </div>
                 <div className='content'>
                 <h2>{ev.title}</h2> 
                 <p>{ev.description}</p> 
